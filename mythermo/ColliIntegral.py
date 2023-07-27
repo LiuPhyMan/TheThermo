d7a14074c214c56f2442c818ae1245b0d0c01b03
LiuPhyMan <liu.jinbao@outlook.com>
2023-07-27 14:04:10 +0800

    07.27.23

--- mythermo/ColliIntegral.py
+++ mythermo/ColliIntegral.py
@@ -1,6 +1,8 @@
-from math import pi, log, sqrt, log10, gamma
+from math import pi, log, sqrt, log10, gamma, exp
 from math import log as ln
+from math import factorial, isclose
 import numpy as np
+from mymath import integrate_by_DCT
 import myconst as const
 from myconst import eV2J, J2eV, K2J, J2K, eV2K, K2eV, relM2absM, e2, epsilon_0
 from myconst import k as kB
@@ -10,16 +12,20 @@ from scipy.constants import k as _kB
 from scipy.constants import m_u as _m_u
 from scipy.interpolate import interp1d
 
-__all__ = ["ExpColli", "LJColli", "MorseColli", "ScreenCoulombColli"]
+__all__ = ["ExpColli", "LJColli", "ScreenCoulombColli"]
 
 _e2 = 2.30708e-28
+
+# import os
+# print(os.path.abspath(__path__))
+from mythermo import path
 
 
 class Spec(object):
 
     def __init__(self, _str):
         self.relM = spec_df.loc[_str, "relM"]
-        self.absM = self.relM * relM2absM
+        self.absM = self.relM*relM2absM
 
 
 # =============================================================================================== #
@@ -28,11 +34,12 @@ class AbsColliIngrl(object):
     def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
         self.spcs = pairs_str.split()
         self.relM = [spec_df.loc[_str, "relM"] for _str in self.spcs]
-        self.absM = [_ * relM2absM for _ in self.relM]
+        self.absM = [_*relM2absM for _ in self.relM]
         self.sumM = sum(self.absM)
-        self.rdcdM = self.absM[0] * self.absM[1] / \
+        self.rdcdM = self.absM[0]*self.absM[1]/ \
                      (self.absM[0] + self.absM[1])
-        self.coeff = [float(_) for _ in coeff_str.split()]
+        # self.coeff = [float(_) for _ in coeff_str.split()]
+        self.coeff_str = coeff_str
         self.style = style
 
     def setReducedT(self, *, T_K: float):
@@ -40,6 +47,7 @@ class AbsColliIngrl(object):
 
     def getColliIntegral(self, *, index: tuple):
         pass
+
 
 class EmptyColli(AbsColliIngrl):
 
@@ -49,6 +57,7 @@ class EmptyColli(AbsColliIngrl):
     def getColliIntegral(self, *, index: tuple):
         return 0
 
+
 # =============================================================================================== #
 class HSColli(AbsColliIngrl):
     r"""
@@ -57,16 +66,17 @@ class HSColli(AbsColliIngrl):
 
     def __init__(self, *, pairs_str, style, coeff_str):
         super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
 
     def getColliIntegral(self, *, index):
-        result = pi * self.coeff[0] ** 2 * \
-                 sqrt(kB * self.rdcdT / 2 / pi / self.rdcdM)
+        result = pi*self.coeff[0]**2* \
+                 sqrt(kB*self.rdcdT/2/pi/self.rdcdM)
         if index == (1, 1):
             return result
         elif index == (1, 2):
-            return 3 * result
+            return 3*result
         elif index == (2, 2):
-            return 2 * result
+            return 2*result
         else:
             raise Exception("Index {} is error".format(index))
 
@@ -79,11 +89,12 @@ class LJColli(AbsColliIngrl):
         colli_coeff "sigma[A]" "epsilon[K]"
         """
         super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
         self._setParas()
 
     def _setParas(self):
-        self.tempValue = pi * self.coeff[0] ** 2 * \
-                         sqrt(kB / 2 / pi / self.rdcdM) * 1e-20
+        self.tempValue = pi*self.coeff[0]**2* \
+                         sqrt(kB/2/pi/self.rdcdM)*1e-20
         data = np.loadtxt("factor/LJ(12-6).txt", delimiter=",")
         # 11, 12, 13, 22, 23, 24
         self.interp_f11 = interp1d(data[:, 0], data[:, 1])
@@ -94,30 +105,111 @@ class LJColli(AbsColliIngrl):
         self.interp_f24 = interp1d(data[:, 0], data[:, 6])
         self.interp_f25 = interp1d(data[:, 0], data[:, 7])
 
-
     def getColliIntegral(self, *, index):
         coeff = 0
         if index == (1, 1):
-            interp_value = self.interp_f11(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f11(self.rdcdT/self.coeff[1])
             coeff = 1
         elif index == (1, 2):
-            interp_value = self.interp_f12(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f12(self.rdcdT/self.coeff[1])
             coeff = 3
         elif index == (1, 3):
-            interp_value = self.interp_f13(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f13(self.rdcdT/self.coeff[1])
             coeff = 12
         elif index == (2, 2):
-            interp_value = self.interp_f22(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f22(self.rdcdT/self.coeff[1])
             coeff = 2
         elif index == (2, 3):
-            interp_value = self.interp_f23(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f23(self.rdcdT/self.coeff[1])
             coeff = 8
         elif index == (2, 4):
-            interp_value = self.interp_f24(self.rdcdT / self.coeff[1])
+            interp_value = self.interp_f24(self.rdcdT/self.coeff[1])
             coeff = 40
         else:
             raise Exception("The index '{}' is error.".format(index))
-        return coeff * self.tempValue * sqrt(self.rdcdT) * interp_value
+        return coeff*self.tempValue*sqrt(self.rdcdT)*interp_value
+
+
+class AbsImpovedLJColli(AbsColliIngrl):
+
+    def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
+        r"""
+        Parameters
+        ----------
+        pairs_str: str
+
+        style: str
+
+        coeff_str: str
+            beta[1], epsilon[meV], r_e[A]
+        Note
+        ----
+        """
+        super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
+        self._path = None
+        self._coef = [None, None]
+        # self._set_paras()
+
+    def _set_paras(self):
+        self.c_mtx_dict = dict()
+        for _index in ("1-1", "1-2", "1-3", "1-4", "1-5", "2-2", "2-3", "2-4", "3-3", "4-4"):
+            self.c_mtx_dict[_index] = np.zeros((7, 3))
+            self.c_mtx_dict[_index][:4, :2] = np.loadtxt(self._path + f"/{_index}.txt",
+                                                         delimiter=",", skiprows=0, max_rows=4)
+            self.c_mtx_dict[_index][4:, :3] = np.loadtxt(self._path + f"/{_index}.txt",
+                                                         delimiter=",", skiprows=4, max_rows=3)
+
+    def get_rdcd_ci(self, *, index: tuple):
+        c_mtx = self.c_mtx_dict[f"{index[0]}-{index[1]}"]
+        a = np.dot(c_mtx, self.coeff[0]**np.arange(3))
+        x = ln(self.rdcdT*K2eV*1e3/self.coeff[1])
+        tmp1 = exp((x - a[2])/a[3])
+        tmp2 = exp((x - a[5])/a[6])
+        ln_rdcd_CI = (a[0] + a[1]*x)*tmp1**2/(tmp1**2 + 1) + \
+                     a[4]*tmp2**2/(tmp2**2 + 1)
+        return exp(ln_rdcd_CI)
+
+    def getColliIntegral(self, *, index: tuple):
+        r"""
+        Return collision integral []
+        """
+        l, s = index[0], index[1]
+        factor = factorial(s + 1)*(2*l + 1 - (-1)**l)/4/(l + 1)
+        # sigma = 0.8002 * self.coeff[0] ** 0.049256 * self.coeff[2]
+        sigma = self._coef[0]*self.coeff[0]**self._coef[1]*self.coeff[2]
+        convert_factor = factor*pi*sigma**2*sqrt(kB*self.rdcdT/2/pi/self.rdcdM)
+        return self.get_rdcd_ci(index=index)*convert_factor * 1e-20
+
+
+class LJm6ColliIntegral(AbsImpovedLJColli):
+
+    def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
+        r"""
+        Parameters
+        ----------
+        pairs_str: str
+
+        style: str
+
+        coeff_str: str
+            beta[1], epsilon[meV], r_e[A]
+        Note
+        ----
+        """
+        super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self._path = path + f"/factor/LJm6"
+        self._coef = [0.8002, 0.049256]
+        super()._set_paras()
+
+
+class LJm4ColliIntegral(AbsImpovedLJColli):
+
+    def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
+        super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self._path = path + f"/factor/LJm4"
+        self._coef = [0.7564, 0.064605]
+        super()._set_paras()
 
 
 # =============================================================================================== #
@@ -132,6 +224,7 @@ class ExpColli(AbsColliIngrl):
         colli_coeff "A[eV]" "rho[A]"
         """
         super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
         self._setParas()
 
     def _setParas(self):
@@ -141,43 +234,76 @@ class ExpColli(AbsColliIngrl):
         self.interp_f22 = interp1d(data[:, 0], data[:, 3])
 
     def getColliIntegral(self, *, index):
-        alpha = log(self.coeff[0] * eV2K / self.rdcdT)
+        alpha = log(self.coeff[0]*eV2K/self.rdcdT)
         assert 3.5 <= alpha <= 28.5, "{}".format(alpha)
-        self.tempValue = 1e-20 * 4 * sqrt(pi * kB * self.rdcdT / 2 / self.rdcdM) * \
-                         alpha ** 2 * self.coeff[1] ** 2
+        self.tempValue = 1e-20*4*sqrt(pi*kB*self.rdcdT/2/self.rdcdM)* \
+                         alpha**2*self.coeff[1]**2
         if index == (1, 1):
-            return self.tempValue * self.interp_f11(alpha)
+            return self.tempValue*self.interp_f11(alpha)
         elif index == (1, 2):
-            return self.tempValue * self.interp_f12(alpha)
+            return self.tempValue*self.interp_f12(alpha)
         elif index == (2, 2):
-            return self.tempValue * self.interp_f22(alpha)
+            return self.tempValue*self.interp_f22(alpha)
         else:
             raise Exception("The index '{}' is error.".format(index))
 
 
-class ChargeExchange(AbsColliIngrl):
+class CEinCI(AbsColliIngrl):
 
     def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
         r"""
         """
         super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
 
     def getColliIntegral(self, *, index: tuple):
         A, B = self.coeff
         l, s = index
-        xi = sum(1 / n for n in range(1, s + 2)) - 0.5772
-        alpha = pi ** 2 / 6 - sum(1 / n ** 2 for n in range(1, s + 2))
-        temp = B ** 2 / 4 * ((ln(2 * kB * self.rdcdT * 1e4 / self.rdcdM) - 2 * A / B + xi)
-                             ** 2 + alpha) * sqrt(kB * self.rdcdT / 2 / pi / self.rdcdM) * 1e-20
+        xi = sum(1/n for n in range(1, s + 2)) - 0.5772
+        alpha = pi**2/6 - sum(1/n**2 for n in range(1, s + 2))
+        temp = B**2/4*((ln(2*kB*self.rdcdT*1e4/self.rdcdM) - 2*A/B + xi)
+                       **2 + alpha)*sqrt(kB*self.rdcdT/2/pi/self.rdcdM)*1e-20
         if index == (1, 1):
-            return temp * 1
+            return temp*1
         elif index == (1, 2):
-            return temp * 3
+            return temp*3
         elif index == (2, 2):
-            return temp * 2
+            return 0
         else:
-            raise Exception("The index '{}' is error.".format(index))
+            return 0
+            # raise Exception("The index '{}' is error.".format(index))
 
+# ----------------------------------------------------------------------------------------------- #
+class CECI(AbsColliIngrl):
+
+    def __init__(self, *, pairs_str: str, style: str, coeff_str: str) -> None:
+        r"""
+
+        Parameters
+        ----------
+        pairs_str
+        style
+        coeff_str
+        """
+        super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        coeff_el, coeff_in = coeff_str.split(",")
+        self.CEel = LJm4ColliIntegral(pairs_str=pairs_str, style="",
+                                      coeff_str=coeff_el.strip())
+        self.CEin = CEinCI(pairs_str=pairs_str, style="",
+                           coeff_str=coeff_in.strip())
+
+    def setReducedT(self, *, T_K: float):
+        super().setReducedT(T_K=T_K)
+        self.CEel.setReducedT(T_K=T_K)
+        self.CEin.setReducedT(T_K=T_K)
+
+    def getColliIntegral(self, *, index: tuple):
+        if isclose(index[0], 1):
+            CI0 = self.CEel.getColliIntegral(index=index)
+            CI1 = self.CEin.getColliIntegral(index=index)
+            return sqrt(CI0**2 + CI1**2)
+        else:
+            return self.CEel.getColliIntegral(index=index)
 
 # class MorseColli(AbsColliIngrl):
 
@@ -257,26 +383,74 @@ class ScreenCoulombColli(AbsColliIngrl):
         http://dx.doi.org/10.1063/1.4821605
         """
         super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self.coeff = [float(_) for _ in coeff_str.split()]
 
     def getColliIntegral(self, *, index: tuple, DebL: float):
         l, s = index
         beta = 1.03
         Z1, Z2 = self.coeff
-        b0 = 1 / 8 / pi / epsilon_0 * abs(Z1 * Z2) * const.e ** 2 / kB / self.rdcdT
-        L = 2 * DebL / beta / b0
+        b0 = 1/8/pi/epsilon_0*abs(Z1*Z2)*const.e**2/kB/self.rdcdT
+        L = 2*DebL/beta/b0
         ##
         # print(f"L = {L}")
         # print(f"temp = {2 * pi * kB * self.rdcdT / self.rdcdM}")
         # print(f"gamma={gamma(s)}")
         ##
-        Psi = 0 if (s == 1) else (sum(1 / n for n in range(1, s)))
-        tmp1 = l * sqrt(2 * pi * kB * self.rdcdT / self.rdcdM) * beta ** 2 * b0 ** 2
+        Psi = 0 if (s == 1) else (sum(1/n for n in range(1, s)))
+        tmp1 = l*sqrt(2*pi*kB*self.rdcdT/self.rdcdM)*beta**2*b0**2
         tmp2 = gamma(s)
-        tmp3 = (ln(L) - l / 2 - 2 * 0.57722 + Psi)
-        return tmp1 * tmp2 * tmp3
+        tmp3 = (ln(L) - l/2 - 2*0.57722 + Psi)
+        return tmp1*tmp2*tmp3
         # return l * sqrt(2 * pi * kB * self.rdcdT / self.rdcdM) * beta ** 2 * b0 ** 2 * gamma(s) * \
         #     (ln(L) - l / 2 - 2 * 0.57722 + Psi)
 
+
+# ----------------------------------------------------------------------------------------------- #
+class EleNeuColliIntegral(AbsColliIngrl):
+
+    def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
+        r"""
+
+        Parameters
+        ----------
+        pairs_str
+        style
+        coeff_str:
+            relative path of momentum transfer cross section file
+
+        Notes
+        -----
+        """
+        super().__init__(pairs_str=pairs_str, style=style, coeff_str=coeff_str)
+        self._set_paras()
+
+    def _set_paras(self):
+        data = np.loadtxt(self.coeff_str, comments="#", delimiter="\t")
+        self.Q_f = interp1d(data[:, 0], data[:, 1], fill_value=0.0,
+                            bounds_error=False)
+
+    def get_rdcd_ci(self):
+        pass
+
+    def getColliIntegral(self, *, index: tuple):
+        def f1(y):
+            s = index[1]
+            x1 = (s + 1)/2*(1 + y)
+            return (s + 1)/2*exp(-x1)*x1**(s + 1)*self.Q_f(x1*self.rdcdT*K2eV)
+
+        def f2(y):
+            s = index[1]
+            if abs(y) < 1e-15:
+                return 0
+            x2 = (s + 1)/abs(y)
+            return (s + 1)/abs(y)**2*exp(-x2)*x2**(s + 1)*self.Q_f(x2*self.rdcdT*K2eV)
+
+        N = 50
+        temp = integrate_by_DCT(f1, N) + 0.5*integrate_by_DCT(f2, N)
+        return 0.5*sqrt(kB*self.rdcdT/2/pi/self.rdcdM)*temp
+
+
+# ----------------------------------------------------------------------------------------------- #
 class TableColliIntegral(AbsColliIngrl):
 
     def __init__(self, *, pairs_str: str, style: str, coeff_str: str):
--- mythermo/Plasma.py
+++ mythermo/Plasma.py
@@ -1,7 +1,12 @@
 import numpy as np
 from myconst import k as kB
+from mymath import coeff1, coeff2
 from .Comp import Composition
-from .ColliIntegral import EmptyColli, ScreenCoulombColli, LJColli
+from .ColliIntegral import (EmptyColli,
+                            LJm6ColliIntegral,
+                            CECI,
+                            EleNeuColliIntegral,
+                            ScreenCoulombColli)
 
 
 class LTEPlasma(object):
@@ -10,6 +15,7 @@ class LTEPlasma(object):
         self.comp = Composition(spcs_str=spcs_str)
         self.p_atm = None
         self.T_K = None
+        self.ci_paras_list = None
 
     def set_pAtm(self, *, p_atm: float) -> None:
         assert p_atm > 0
@@ -41,25 +47,53 @@ class LTEPlasma(object):
                     CI_engine[i].append(EmptyColli(pairs_str=pairs_str, style="empty",
                                                    coeff_str=""))
                 else:
-                    if ci_paras[1] == "SC":
-                        CI_engine[i].append(ScreenCoulombColli(pairs_str=ci_paras[0],
-                                                               style="SC",
-                                                               coeff_str=ci_paras[2]))
-                    elif ci_paras[1] == "LJ":
-                        CI_engine[i].append(LJColli(pairs_str=ci_paras[0],
-                                                    style="LJ",
-                                                    coeff_str=ci_paras[2]))
-                    else:
-                        raise Exception("")
+                    match ci_paras[1]:
+                        case "SC":
+                            CI_engine[i].append(ScreenCoulombColli(pairs_str=ci_paras[0],
+                                                                   style="SC",
+                                                                   coeff_str=ci_paras[2]))
+                        case "CE":
+                            CI_engine[i].append(CECI(pairs_str=ci_paras[0],
+                                                     style="CE",
+                                                     coeff_str=ci_paras[2]))
+                        case "LJm6":
+                            CI_engine[i].append(LJm6ColliIntegral(pairs_str=ci_paras[0],
+                                                                  style="LJm6",
+                                                                  coeff_str=ci_paras[2]))
+                        case "EN":
+                            CI_engine[i].append(EleNeuColliIntegral(pairs_str=ci_paras[0],
+                                                                    style="EN",
+                                                                    coeff_str=ci_paras[2]))
+                        case _:
+                            raise Exception(ci_paras[1])
         self.ci_engine = CI_engine
 
-    def get_Omega_ij(self, *, iRow:int, jCol:int, index:tuple) -> float:
+    def mu_ij(self, iRow: int, jCol: int):
+        return self.comp.absM[iRow]*self.comp.absM[jCol]/(
+                self.comp.absM[iRow] + self.comp.absM[jCol])
+
+    def Omega_ij(self, iRow: int, jCol: int, *, index: tuple) -> float:
         self.ci_engine[iRow][jCol].setReducedT(T_K=self.T_K)
         if isinstance(self.ci_engine[iRow][jCol], ScreenCoulombColli):
+            # for screen coulomb interaction
             return self.ci_engine[iRow][jCol].getColliIntegral(index=index, DebL=self.comp.DebL())
         else:
             return self.ci_engine[iRow][jCol].getColliIntegral(index=index)
 
+    def Aij(self, i, j) -> float:
+        r""" i: index of Row; j: index of Col """
+        return 0.5*self.Omega_ij(i, j, index=(2, 2))/self.Omega_ij(i, j, index=(1, 1))
+
+    def Bij(self, i, j) -> float:
+        return 1/3*(5*self.Omega_ij(i, j, index=(1, 2)) - self.Omega_ij(i, j, index=(1, 3)))/ \
+            self.Omega_ij(i, j, index=(1, 1))
+
+    def Cij(self, i, j):
+        return 1/3*self.Omega_ij(i, j, index=(1, 2))/self.Omega_ij(i, j, index=(1, 1))
+
+    # ------------------------------------------------------------------------------------------- #
+    # Cal viscosity
+    # ------------------------------------------------------------------------------------------- #
     def get_H_mtx(self, *, p, q):
         # Init
         A1 = np.zeros((self.comp.n_spcs, self.comp.n_spcs))
@@ -69,19 +103,19 @@ class LTEPlasma(object):
         # Set A1, A2.
         for iRow in range(self.comp.n_spcs):
             for jCol in range(self.comp.n_spcs):
-                M1 = self.comp.relM[iRow] / (self.comp.relM[iRow] +self.comp.relM[jCol])
-                M2 = self.comp.relM[jCol] / (self.comp.relM[iRow] +self.comp.relM[jCol])
+                M1 = self.comp.relM[iRow]/(self.comp.relM[iRow] + self.comp.relM[jCol])
+                M2 = self.comp.relM[jCol]/(self.comp.relM[iRow] + self.comp.relM[jCol])
                 self.ci_engine[iRow][jCol].setReducedT(T_K=self.T_K)
                 tmp = 0
                 for _c in coeff1(p, q):
-                    omega = self.get_Omega_ij(iRow=iRow, jCol=jCol, index=_c[2])
-                    tmp += _c[0] * (M1**_c[1][0]) * (M2**_c[1][1]) * omega
-                A1[iRow, jCol] = tmp * self.comp.xj[iRow] * self.comp.xj[jCol]
+                    omega = self.Omega_ij(iRow=iRow, jCol=jCol, index=_c[2])
+                    tmp += _c[0]*(M1**_c[1][0])*(M2**_c[1][1])*omega
+                A1[iRow, jCol] = tmp*self.comp.xj[iRow]*self.comp.xj[jCol]
                 tmp = 0
                 for _c in coeff2(p, q):
-                    omega = self.get_Omega_ij(iRow=iRow, jCol=jCol, index=_c[2])
-                    tmp += _c[0] * (M1**_c[1][0]) * (M2**_c[1][1]) * omega
-                A2[iRow, jCol] = tmp * self.comp.xj[iRow] * self.comp.xj[jCol]
+                    omega = self.Omega_ij(iRow=iRow, jCol=jCol, index=_c[2])
+                    tmp += _c[0]*(M1**_c[1][0])*(M2**_c[1][1])*omega
+                A2[iRow, jCol] = tmp*self.comp.xj[iRow]*self.comp.xj[jCol]
         # --------------------------------------------------------------------------------------- #
         # Set H matrix from A1, A2.
         for iRow in range(self.comp.n_spcs):
@@ -89,85 +123,42 @@ class LTEPlasma(object):
                 H[iRow, jCol] = A2[iRow, jCol]
                 if iRow == jCol:
                     H[iRow, jCol] += np.sum(A1[iRow])
-        H = H * 2 / 5 / kB / self.T_K
+        H = H*2/5/kB/self.T_K
         return H
 
-    def get_eta(self, *, T_K:float) -> float:
+    def get_eta_order_2(self, *, T_K: float) -> float:
         H00 = self.get_H_mtx(p=0, q=0)
         H01 = self.get_H_mtx(p=0, q=1)
         H11 = self.get_H_mtx(p=1, q=1)
         b20 = np.linalg.solve(H00 - np.dot(H01.dot(np.linalg.inv(H11)), H10), 2/kB/T*self.comp.xj)
-        return 1 / 2 * kB * T * np.dot(self.comp.xj, b20)
+        return 1/2*kB*T*np.dot(self.comp.xj, b20)
 
-def coeff1(p, q):
-    if (p, q) == (0, 0):
-        return [[80 / 3, (1, 1), (1, 1)],
-                [8, (0, 2), (2, 2)]]
-    elif (p, q) == (0, 1):
-        return [[280 / 3, (1, 2), (1, 1)],
-                [-112 / 3, (1, 2), (1, 2)],
-                [28, (0, 3), (2, 2)],
-                [-8, (0, 3), (2, 3)]]
-    elif (p, q) == (1, 1):
-        return [[560 / 3, (3, 1), (1, 1)],
-                [980 / 3, (1, 3), (1, 1)],
-                [-784 / 3, (1, 3), (1, 2)],
-                [128 / 3, (1, 3), (1, 3)],
-                [308 / 3, (2, 2), (2, 2)],
-                [294 / 3, (0, 4), (2, 2)],
-                [-56, (0, 4), (2, 3)],
-                [8, (0, 4), (2, 4)],
-                [16, (1, 3), (3, 3)]]
-    else:
-        raise Exception("")
+    def get_eta_order_1(self, *, T_K: float) -> float:
+        H00 = self.get_H_mtx(p=0, q=0)
+        b10 = np.linalg.solve(H00, 2/kB/T_K*self.comp.xj)
+        return 1/2*kB*T_K*np.dot(self.comp.xj, b10)
 
-def coeff2(p, q):
-    if (p, q) == (0, 0):
-        return [[-80 / 3, (1, 1), (1, 1)],
-                [8, (1, 1), (2, 2)]]
-    elif (p, q) == (0, 1):
-        return [[-280 / 3, (2, 1), (1, 1)],
-                [112 / 3, (2, 1), (1, 2)],
-                [28, (2, 1), (2, 2)],
-                [-8, (2, 1), (2, 3)]]
-    elif (p, q) == (1, 1):
-        return [[-1540 / 3, (2, 2), (1, 1)],
-                [784 / 3, (2, 2), (1, 2)],
-                [-128 / 3, (2, 2), (1, 3)],
-                [602 / 3, (2, 2), (2, 2)],
-                [-56, (2, 2), (2, 3)],
-                [8, (2, 2), (2, 4)],
-                [-16, (2, 2), (3, 3)]]
-    else:
-        raise Exception("")
+    # ------------------------------------------------------------------------------------------- #
+    def _mu_ij(self, i, j):
+        return self.comp.absM[i]*self.comp.absM[j]/(self.comp.absM[i] + self.comp.absM[j])
 
+    def _k_ij(self, i, j):
+        return 75/64*kB**2*self.T_K/self._mu_ij(i, j)/self.Omega_ij(i, j, index=(2, 2))
 
+    def get_L_mtx(self):
+        L = np.zeros((self.comp.n_spcs, self.comp.n_spcs))
+        for i in range(self.comp.n_spcs):
+            tmp = 0
+            for k in range(self.comp.n_spcs):
+                if k == iRow:
+                    continue
+                mu_i = self.comp.absM[i]/(self.comp.absM[i] + self.comp.absM[k])
+                mu_k = self.comp.absM[k]/(self.comp.absM[i] + self.comp.absM[k])
+                tmp1 = np.dot([15/2, 25/4, -3, 4],
+                              [mu_i**2, mu_k**2,
+                               mu_k**2*self.Bij(i, k), mu_i*mu_k*self.Aij(i, k)])
+                tmp = tmp + 2*self.comp.xj[k]/self._k_ij(i, k)*tmp1/self.Aij(i, k)
+            L[i, i] = -4*self.comp.xj[i]/self._k_ij(i, i) - tmp
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def get_k_hp(self):
+        pass
